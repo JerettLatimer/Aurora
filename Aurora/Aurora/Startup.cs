87a25f668e0eb44b5c8ff1187d725a184941231d
Matthew Jett <matthew.jett@eagles.ewu.edu>
2020-02-16 00:25:34 -0800

    added new cshtml pages, Map, Group, About, and Task. Removed page Privacy and tweaked _Layout  to reflect linking to new pages. Added in the classes needed for the Tasker, Mapper, and Mailer into a new solution space folder called, Parts. Added in ArcGIS API for JavaScript sdk ino the wwwroot solution space of the project. Project feels ready for this to become it's base master branch template.

--- Aurora/Aurora/Startup.cs
+++ Aurora/Aurora/Startup.cs
@@ -9,48 +9,55 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace Aurora
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		#region Properties
+		public IConfiguration Configuration { get; }
+		#endregion
 
-        public IConfiguration Configuration { get; }
+		#region Constructor
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+		#endregion
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-        }
+		#region Methods
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddRazorPages();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapRazorPages();
+			});
+		}
+		#endregion
+	}
 }
