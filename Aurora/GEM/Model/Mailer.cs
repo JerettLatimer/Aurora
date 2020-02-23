baa5a1d5a02c6b70d0e8599481fda4e324196429
Matthew Jett <matthew.jett@eagles.ewu.edu>
2020-02-23 14:22:08 -0800

    added sample ArcGIS Online map to map page. added overflow-y attribute to site.css to prevent from pages being pushed over to left when scrollbar appears. renamed /Parts folder to /Model and adjusted namespaces for associated classes within.

--- Aurora/GEM/Model/Mailer.cs
+++ Aurora/GEM/Model/Mailer.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GEM.Parts
+namespace GEM.Model
 {
-	public class Mapper
+	public class Mailer
 	{
 	}
 }
--- Aurora/GEM/Model/Tasker.cs
+++ Aurora/GEM/Model/Tasker.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GEM.Parts
+namespace GEM.Model
 {
-	public class Mailer
+	public class Tasker
 	{
 	}
 }
