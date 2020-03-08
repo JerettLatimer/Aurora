da0a4e171599693ba9d255e5f3db692ab0386cb6
JerettLatimer <jerettlatimer@yahoo.com>
2020-03-08 14:32:18 -0700

    added dynamic table updating and styling

--- Aurora/GEM/Pages/Task.cshtml.cs
+++ Aurora/GEM/Pages/Task.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GEM.Model;
 using HtmlTags;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,77 +12,36 @@ namespace GEM
 {
     public class TaskModel : PageModel
     {
-        // List property will be reference to Fetcher data property
-        public List<List<string>> TestList { get; set; } = new List<List<string>>();
-
-        internal void fillList()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                TestList.Add(new List<string>());
-                TestList.ElementAt(i).Add("HEADER");
-                for (int j = 0; j < 10; j++)
-                {
-                    TestList.ElementAt(i).Add("data");
-                }
-            }
-        }
-
+        private List<Geodata> Routers { get; } = Fetcher.Instance.GeodataRouters.sites;
+        HtmlTag table, headerRow;
         public HtmlTag generateTable()
         {
-            this.createJson();
-            this.fillList();
-            HtmlTag table = new HtmlTag("table");
-            HtmlTag headerRow = new HtmlTag("tr");
-            HtmlTag dataRow = new HtmlTag("tr");
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    if (j == 0)
-                    {
-                        headerRow.Add("th")
-                            .Text(TestList.ElementAt(i).ElementAt(j));
-                    }
-                    else
-                    {
-                        dataRow.Add("td")
-                            .Text(TestList.ElementAt(i).ElementAt(j));
-                    }
-                }
-                
-            }
+            table = new HtmlTag("table").AddClass("table");
+            headerRow = new HtmlTag("tr");
+
+            headerRow.Add("th").Text("ID");
+            headerRow.Add("th").Text("CGR Name");
+            headerRow.Add("th").Text("Status");
+            headerRow.Add("th").Text("Latitude");
+            headerRow.Add("th").Text("Longitude");
             table.AppendHtml(headerRow.ToHtmlString());
-            table.AppendHtml(dataRow.ToHtmlString());
+
+            foreach (Geodata router in Routers)
+            {
+                HtmlTag row = new HtmlTag("tr");
+                row.Add("td").Text(router._id.ToString());
+                row.Add("td").Text(router.name);
+                row.Add("td").Text(router.status);
+                row.Add("td").Text(router.location.coordinates.latitude.ToString());
+                row.Add("td").Text(router.location.coordinates.longitude.ToString());
+                table.AppendHtml(row.ToHtmlString());
+            }
             return table;
-        }
-
-        private void createJson()
-        {
-            TestJson testJson = new TestJson();
-            testJson.FirstName = "Jerett";
-            testJson.LastName = "Latimer";
-            testJson.StreetAddress = "209 Brown Rd E";
-            testJson.City = "Chehalis";
-            testJson.State = "WA";
-            testJson.Age = 23;
-
-            string json = JsonConvert.SerializeObject(testJson);
         }
 
         public void OnGet()
         {
 
-        }
-
-        internal class TestJson
-        {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string StreetAddress { get; set; }
-            public string City { get; set; }
-            public string State { get; set; }
-            public int Age { get; set; }
         }
     }
 }
