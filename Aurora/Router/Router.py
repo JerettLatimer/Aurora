85454f42a61ae48c2a2776ee057ff7d0314aed0f
Luna Lord <lunalord@eagles.ewu.edu>
2020-04-19 18:57:40 -0700

    Inital commit of Router python script

--- Aurora/Router/Router.py
+++ Aurora/Router/Router.py
@@ -1,1 +1,37 @@
+import pymongo as pm
+import time
+import signal
+import geojson
+
+location = geojson.Point((117.147, 47.3955))
+
+data = {
+        "name": "rp1",
+        "status": "",
+        "location": location
+}
+
+
+def term_handler(signal_num, stack_num):
+    collection.delete_one({"name": "rp1"})
+    data["status"] = "offline"
+    collection.insert_one(data)
+    print("Signal Number: " + signal_num + "" + stack_num)
+
+
+client = pm.MongoClient("mongodb+srv://LunaLord:1234@cluster0-liruv.azure.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test_cgr
+collection = db.test_monitor
+
+signal.signal(signal.SIGTERM, term_handler)
+
+while True:
+    collection.delete_one({"name": "rp1"})
+    data["status"] = "online"
+    print(data)
+    collection.insert_one(data)
+    time.sleep(10)
+
+
+
 
