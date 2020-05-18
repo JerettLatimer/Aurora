400582d256018c8eb53dfe41f986a5e06d2a71d6
Matthew Jett <matthew.jett@eagles.ewu.edu>
2020-05-17 21:29:56 -0700

    total redesign of Tasks page: rule table, task menu and css styling transitions. Template ready for Subscriptions page. Only thing left to do here for Tasks page is to make last Task menu buttons to submit checked rules and selected Subscription into the selected/current Task properties.

--- Aurora/GEM/Pages/TasksPage.cshtml.cs
+++ Aurora/GEM/Pages/TasksPage.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Aurora;
 using GEM.Model;
 using HtmlTags;
 using Microsoft.AspNetCore.Mvc;
@@ -11,49 +12,13 @@ using Newtonsoft.Json;
 
 namespace GEM
 {
-    public class TasksPageModel : PageModel
-    {
-        readonly Site _survey = Fetcher.GetGeodataListAsync().Result;
-        HtmlTag _table, _headerRow;
-
-
-        //public HtmlTag BuildRules()
-        //{
-        //    _table = new HtmlTag("table").AddClass("table");
-        //    //_headerRow = new HtmlTag("tr");
-
-        //    //_headerRow.Add("th").Text("ID");
-        //    //_headerRow.Add("th").Text("CGR Name");
-        //    //_headerRow.Add("th").Text("Status");
-        //    //_headerRow.Add("th").Text("Latitude");
-        //    //_headerRow.Add("th").Text("Longitude");
-        //    //_table.AppendHtml(_headerRow.ToHtmlString());
-
-        //    foreach (PropertyInfo property in _survey.Properties) {
-        //        var row = new HtmlTag("tr").AddClass("fieldRow");
-
-        //        row.Add("")
-        //        row.Add("td").AddClass("").Text(property.Name);
-        //        row.Add("td").Text(router.name);
-        //        if (router.status.Equals("online")) {
-        //            row.Add("td").Text(router.status).Style("color", "green");
-        //        }
-        //        else if (router.status.Equals("offline")) {
-        //            row.Add("td").Text(router.status).Style("color", "red");
-        //        }
-        //        else {
-        //            row.Add("td").Text(router.status).Style("color", "orange");
-        //        }
-        //        row.Add("td").Text(router.location.coordinates.latitude.ToString());
-        //        row.Add("td").Text(router.location.coordinates.longitude.ToString());
-        //        _table.AppendHtml(row.ToHtmlString());
-        //    }
-        //    return _table;
-        //}
-
-        public void OnGet()
-        {
-
-        }
-    }
+	public class TasksPageModel : PageModel
+	{
+		public List<GEM.Model.Task> DEMOTASKS = Fetcher._DEMO_TASKS;
+		public List<Subscription> DEMOSUBSCRIPTIONS = Fetcher._DEMO_SUBSCRIPTIONS;
+		public Site SITE = Fetcher.Survey;
+	}
 }
+
+// TODO: pass selection of user 'selected subscription' into currently selected Task's property as well as the 'selected rules' into it's property...
+// TODO: create submit button and any other buttons needed for Task menu...
--- Aurora/GEM/wwwroot/js/site.js
+++ Aurora/GEM/wwwroot/js/site.js
@@ -1,4 +1,81 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
 // Write your Javascript code.
+
+
+function RadioConditionCheck() {
+    for (var radio in $(".condition").children) {
+        if (radio.checked) {
+            if (radio === document.getElementById("anyChnage")) {
+                console.log("inside anyChnage");
+            }
+            //else if (radio.$('#isConditional')) {
+            //    $('.radioCondition').after(
+            //        <div className="dropdown">
+            //            <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+            //                <span className="caret"></span>
+            //            </button>
+            //            <ul className="dropdown-menu">
+            //                <li><a href="#">HTML</a></li>
+            //                <li><a href="#">CSS</a></li>
+            //                <li><a href="#">JavaScript</a></li>
+            //            </ul>
+            //        </div>,
+            //        <input type="text"></input>
+            //    );
+            //}
+            //TasksPage.Test();
+        }
+    }
+}
+
+/*$(document).ready(function () {
+    $('.createColumn_neworcancelButton').click(function () {
+        
+    });
+});*/
+
+
+/* Rule checkbox toggle for show/hide of radio buttons */
+$(".fieldCondition").hide();
+function toggleConditions(prop) {
+    if ($(".rule" + prop).is(":checked")) {
+        $(".condition" + prop).show(300);
+    }
+    else {
+        $(".condition" + prop).hide(200);
+    }
+};
+
+/* Custom Dropdowns */
+// Ref: https://codepen.io/hmps/pen/CbltK
+$(".dropdownGem .dropdownGem-title").click(function () {
+    if ($('.dropdownGem-menu').height() > 0) {
+        closeMenu(this);
+    } else {
+        openMenu(this);
+    }
+});
+
+$(".dropdownGem-menu li").click(function () {
+    closeMenu(this);
+});
+
+function closeMenu(el) {
+    $(el).closest('.dropdownGem').toggleClass("closed").find(".selection").text($(el).text());
+    $('.dropdownGem-menu ul').css("top", 0);
+}
+
+function openMenu(el) {
+    $(el).parent().toggleClass("closed");
+    $('.dropdownGem-menu').css({
+        height: 'auto'
+    }).mousemove(function (e) {
+        var heightDiff = $('.dropdownGem-menu ul').height() / $('.dropdownGem-menu').height(),
+            offset = $('.dropdownGem-menu').offset(),
+            relativeY = (e.pageY - offset.top),
+            top = relativeY * heightDiff > $('.dropdownGem-menu ul').height() - $('.dropdownGem-menu').height() ?
+                $('.dropdownGem-menu ul').height() - $('.dropdownGem-menu').height() : relativeY * heightDiff;
+        $('.dropdownGem-menu ul').css("top", -top);
+    });
+}
